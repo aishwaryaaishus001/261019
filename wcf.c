96f6418087f0d3887b74d8790ecd729436f5be50
aishwaryaaishus001 <69378925+aishwaryaaishus001@users.noreply.github.com>
2021-04-26 17:06:30 +0530

    Add files via upload

---
+++ wcf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+  float v,t,wcf;
+  printf("Enter the wind velocity: ");
+  scanf("%f",&v);
+  printf("Enter the temperature: ");
+  scanf("%f",&t);
+  wcf = 35.74 + (0.6215*t) + (((0.4275*t) - 35.75) * pow(v,0.16));
+  printf("WIND CHILL FACTOR = %f\n",wcf);
+}
