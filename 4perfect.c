19e165e0e8de6b28db6fc559f001f0953e9a53b0
aishwaryaaishus001 <69378925+aishwaryaaishus001@users.noreply.github.com>
2021-04-26 16:29:23 +0530

    Add files via upload

---
+++ 4perfect.c
@@ -0,0 +1,28 @@
+/*
+	PROGRAM TO PRINT THE FIRST 4 PERFECT NUMBERS.
+	AUTHOR : AISHWARYA KP.
+*/
+#include<stdio.h>
+void main()
+{
+	int n=1,i,sum,count=0;
+	while(count<4)
+	{
+		i=1;
+		sum=0;
+		while(i<n)
+		{
+			if(n%i==0)
+			{
+				sum=sum+i;
+			}
+			i++;
+		}
+		if(sum==n)
+		{
+			printf("%d\n",n);
+			count++;
+		}
+		n++;
+	}
+}
